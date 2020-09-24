26eb581085d95978bec269a31b2b8f463b095394
Bianca <biancascampos0410@gmail.com>
2020-09-24 16:56:19 -0300

    AJ1 Parametros

---
+++ Parametros/src/br/com/epiousion/ParametrosMain.java
@@ -0,0 +1,23 @@
+package br.com.epiousion;
+
+public class ParametrosMain {
+    public static void main(String[] args) {
+        if (args.length < 5) {
+            System.out.println("O uso correto da classe é");
+            System.out.println("java ParametrosMain <nome> <data nascimento> <rg> <sexo> <salario>");
+        } else {
+            String nome = args[0];
+            String dataNascimento = args [1];
+            String rg= args[2];
+            String salario = args[3];
+            String sexo = args[4];
+            String textNome = "O(a) Senhor(a) " + nome;
+            String textNasc = ", nascido(a) em " + dataNascimento;
+            String textRg = " portador(a)do rg de numero " + rg + ", \n";
+            String textSexo = " do sexo " + sexo;
+            String textTrabalho = ", está registrado(a) com o salário de R$ " + salario
+                    + ".";
+            System.out.println(textNome + textRg + textNasc + textSexo + textTrabalho);
+        }
+    }
+}
